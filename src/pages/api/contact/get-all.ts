c68f186134cddbf3e60ae9818f9b7fad01fd5f88
Igor Lourenço <theigorlourenco@gmail.com>
2020-11-19 15:03:52 -0300

    Create Contacts List API (Bugfix)

--- src/pages/api/contact/get-all.ts
+++ src/pages/api/contact/get-all.ts
@@ -1,11 +1,11 @@
-import {NowResponse} from '@vercel/node'
+import {NowRequest, NowResponse} from '@vercel/node'
 import connectToDatabase from '../../../services/database'
 
-export default async (response: NowResponse) => {
+export default async (request: NowRequest, response: NowResponse) => {
     const db = await connectToDatabase(process.env.MONGODB_URI)
     const collection = db.collection('contacts')
 
     const contacts =  await collection.find({}).toArray()
 
-    return response.status(201).json(contacts)
+    return response.status(200).send(contacts)
 }
