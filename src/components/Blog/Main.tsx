d7675e357b9b0d93bfce6a9755cfc1292726ba6e
Igor Lourenço <theigorlourenco@gmail.com>
2020-11-20 12:58:24 -0300

    Add Pagination to Articles Page

--- src/components/Blog/Main.tsx
+++ src/components/Blog/Main.tsx
@@ -1,18 +1,22 @@
-import {useFetch} from '../../services/swr';
-import Loading from '../message/Loading';
-import {Heading} from "@chakra-ui/layout";
+import {useFetch} from '../../services/swr'
+import Loading from '../message/Loading'
+import {Heading} from "@chakra-ui/layout"
+import MainProps from '../../interfaces/MainProps'
 
-const Main = () => {
-    const {data} = useFetch('api/article/get-all/?page=1')
+const Main = (props: MainProps) => {
+    const {data} = useFetch(`api/article/get-all/?page=${props.page}`)
 
     if (!data) return <Loading/>
 
     return (
-        data.map(article => (
-            <Heading marginY={12}>
-                {article.title}
+        <>
+            {data.map(article => (
+            <Heading key={article._id} marginY={12}>
+                {article.author}
             </Heading>
-        ))
+
+            ))}
+        </>
     )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,24 @@ import {Box} from '@chakra-ui/core'
 import Layout from '../components/Layout'
 import Header from '../components/Header'
 import Main from '../components/Blog/Main'
+import {useState} from 'react'
+import {Button} from '@chakra-ui/button'
 
-const Index = () => (
-    <Layout>
-        <Box>
-            <Header/>
-            <Main/>
-        </Box>
-    </Layout>
-)
+const Index = () => {
+    const [page, setPage] = useState(1)
+
+    return (
+        <Layout>
+            <Box>
+                <Header/>
+                <Main page={page}/>
+                <Box flex={`1`}>
+                    <Button onClick={()=>setPage(page + 1)}>+1</Button>
+                    <Button onClick={()=>setPage(page -1 )}>-1</Button>
+                </Box>
+            </Box>
+        </Layout>
+    )
+}
 
 export default Index
