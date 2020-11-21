49304978889ee77efbd8e2e73c49285542a58a8f
Igor Lourenço <theigorlourenco@gmail.com>
2020-11-21 18:12:37 -0300

    Get One Article Data to Show on Page

--- src/pages/api/article/find-one.ts
+++ src/pages/api/article/find-one.ts
@@ -1,5 +1,6 @@
 import {NowRequest, NowResponse} from '@vercel/node'
 import connectToDatabase from '../../../services/database'
+import {ObjectId} from 'mongodb'
 
 export default async (request: NowRequest, response: NowResponse) => {
     const {article} = request.query
@@ -7,7 +8,7 @@ export default async (request: NowRequest, response: NowResponse) => {
     const db = await connectToDatabase(process.env.MONGODB_URI)
     const collection = db.collection('articles')
 
-    const articles = await collection.findOne({_id: article})
+    const articles = await collection.findOne({_id: new ObjectId(article.toString())})
 
     return response.status(200).send(articles)
 }
--- src/pages/article/[article].tsx
+++ src/pages/article/[article].tsx
@@ -1,16 +1,17 @@
 import { useRouter } from 'next/router'
 import {useFetch} from "../../services/swr";
 import Loading from "../../components/message/Loading";
+import {useState} from "react";
 
 const Article = () => {
     const router = useRouter()
     const { article } = router.query
 
-    const {data} = useFetch(`api/article/find-one/?article=${article}`)
+    const {data} = useFetch(`/api/article/find-one/?article=${article}`)
 
     if (!data) return <Loading/>
 
-    return <p>Post: {data.articles.title}</p>
+    return <p>Post: {data.title}</p>
 }
 
 export default Article
